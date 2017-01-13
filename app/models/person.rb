689843b320d253232e0353c15fe8db38aba4c568
dalai <dav1a1223@gmail.com>
2017-01-13 17:38:23 +0800

    build association

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,3 +1,9 @@
 class Person < ApplicationRecord
   belongs_to :country
+  has_many :directors, foreign_key: :person_id
+  has_many :direct_movies, through: :directors, source: :movie
+  has_many :actors, foreign_key: :person_id
+  has_many :actor_movies, through: :actors, source: :movie
+  has_many :writers, foreign_key: :person_id
+  has_many :writer_movies, through: :writers, source: :movie
 end

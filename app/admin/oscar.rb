cef02fb444dd27826612eaecddccada9ef40cfb0
relaxlion <b02208031@ntu.edu.tw>
2017-01-16 01:37:48 +0800

    pride empty

--- app/admin/oscar.rb
+++ app/admin/oscar.rb
@@ -24,44 +24,84 @@ permit_params :year, :best_picture_id, :best_supporting_actor_id, :best_supporti
 		attributes_table do
 			row :id
 			row :best_picture do
-				movie = Movie.find(oscar.best_picture_id)
-				link_to "#{movie.chinese_name}", admin_movie_path(id: oscar.best_picture.id)
+				begin
+					movie = Movie.find(oscar.best_picture_id)
+				rescue
+					movie = nil
+				end
+					link_to "#{movie.chinese_name}", admin_movie_path(id: oscar.best_picture.id) if movie.present?
 			end
 			row :best_director do
-				director = Person.find(Director.find(oscar.best_director_id).person_id)
-				link_to "#{director.chinese_name}", admin_director_path(id: oscar.best_director.id)
+				begin
+					director = Person.find(Director.find(oscar.best_director_id).person_id)
+				rescue
+					director = nil
+				end
+					link_to "#{director.chinese_name}", admin_director_path(id: oscar.best_director.id)if director.present?
 			end
 			row :best_actor do
-				actor = Person.find(Actor.find(oscar.best_actor_id).person_id)
-				link_to "#{actor.chinese_name}", admin_actor_path(id: oscar.best_actor.id)
+				begin
+					actor = Person.find(Actor.find(oscar.best_actor_id).person_id)
+				rescue
+					actor = nil
+				end
+					link_to "#{actor.chinese_name}", admin_actor_path(id: oscar.best_actor.id)if actor.present?
 			end
 			row :best_actress do
-				actor = Person.find(Actor.find(oscar.best_actress_id).person_id)
-				link_to "#{actor.chinese_name}", admin_actor_path(id: oscar.best_actress.id)
+				begin
+					actor = Person.find(Actor.find(oscar.best_actress_id).person_id)
+				rescue
+					actor = nil
+				end
+					link_to "#{actor.chinese_name}", admin_actor_path(id: oscar.best_actress.id)if actor.present?
 			end
 			row :best_supporting_actor do
-				actor = Person.find(Actor.find(oscar.best_supporting_actor_id).person_id)
-				link_to "#{actor.chinese_name}", admin_actor_path(id: oscar.best_supporting_actor.id)
+				begin
+					actor = Person.find(Actor.find(oscar.best_supporting_actor_id).person_id)
+				rescue
+					actor = nil
+				end
+					link_to "#{actor.chinese_name}", admin_actor_path(id: oscar.best_supporting_actor.id)if actor.present?
 			end
 			row :best_supporting_actress do
-				actor = Person.find(Actor.find(oscar.best_supporting_actress_id).person_id)
-				link_to "#{actor.chinese_name}", admin_actor_path(id: oscar.best_supporting_actress.id)
+				begin
+					actor = Person.find(Actor.find(oscar.best_supporting_actress_id).person_id)
+				rescue
+					actor = nil
+				end
+					link_to "#{actor.chinese_name}", admin_actor_path(id: oscar.best_supporting_actress.id)if actor.present?
 			end
 			row :best_animated_feature_flim do
-				movie = Movie.find(oscar.best_animated_feature_flim_id)
-				link_to "#{movie.chinese_name}", admin_movie_path(id: oscar.best_animated_feature_flim.id)
+				begin
+					movie = Movie.find(oscar.best_animated_feature_flim_id)
+				rescue
+					movie = nil
+				end
+					link_to "#{movie.chinese_name}", admin_movie_path(id: oscar.best_animated_feature_flim.id)if movie.present?
 			end
 			row :best_foreign_language_flim do
-				movie = Movie.find(oscar.best_animated_feature_flim_id)
-				link_to "#{movie.chinese_name}", admin_movie_path(id: oscar.best_animated_feature_flim.id)
+				begin
+					movie = Movie.find(oscar.best_animated_feature_flim_id)
+				rescue
+					movie = nil
+				end
+					link_to "#{movie.chinese_name}", admin_movie_path(id: oscar.best_animated_feature_flim.id)if movie.present?
 			end
 			row :best_original_screenplay do
-				 movie = Movie.find(Writer.find(oscar.best_original_screenplay_id).movie_id)
-				link_to "#{movie.chinese_name}", admin_writer_path(id: oscar.best_original_screenplay.id)
+				begin
+				 	movie = Movie.find(Writer.find(oscar.best_original_screenplay_id).movie_id)
+				rescue
+					movie = nil
+				end
+					link_to "#{movie.chinese_name}", admin_writer_path(id: oscar.best_original_screenplay.id)if movie.present?
 			end
 			row :best_adapted_screenplay do
-				movie = Movie.find(Writer.find(oscar.best_adapted_screenplay_id).movie_id)
-				link_to "#{movie.chinese_name}", admin_writer_path(id: oscar.best_adapted_screenplay.id)
+				begin
+					movie = Movie.find(Writer.find(oscar.best_adapted_screenplay_id).movie_id)
+				rescue
+					movie = nil
+				end
+					link_to "#{movie.chinese_name}", admin_writer_path(id: oscar.best_adapted_screenplay.id)if movie.present?
 			end
 
 		end
